b66ff2d14763ab088376801940da31154e170ec5
Tobin Yehle <tobinyehle@gmail.com>
2020-05-03 22:54:34 -0700

    add an executable

--- src/speakdata/__main__.py
+++ src/speakdata/__main__.py
@@ -1,2 +1,16 @@
+import sys
+
+import speakdata
+
+
 def console_entry() -> None:
-    pass
+    if len(sys.argv) >= 2:
+        with open(sys.argv[1], "rb") as handle:
+            data = handle.read()
+    else:
+        data = sys.stdin.buffer.read()
+    print(speakdata.pronounce(data))
+
+
+if __name__ == "__main__":
+    console_entry()
